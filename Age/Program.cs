a13be4b6413eac470a80cfa0fea0d7a7c4d3fe7f
Frederik Hvid Sørensen <fred09j3@zbc.dk>
2022-03-21 13:44:46 +0100

    Final answer

--- Age/Program.cs
+++ Age/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name, input;
+byte age;
+
+Console.WriteLine("Indtast dit navn: ");
+name = Console.ReadLine();
+
+do
+{
+    Console.WriteLine("Indtast din alder: ");
+    input = Console.ReadLine();
+}
+while (!byte.TryParse(input, out age));
+
+if (age < 3) // age < 3
+{
+    Console.WriteLine($"{name}, du må gå med ble.");
+}
+else if (age > 3 && age <= 15) // Age between 3 and 15
+{
+    Console.WriteLine($"{name}, du må ingenting.");
+}
+else if (age > 15 && age < 18) // Age between 15 and 18 (15 and 18 excluded)
+{
+    Console.WriteLine($"{name}, du må drikke.");
+}
+else // Age 18 and above
+{
+    Console.WriteLine($"{name}, du må stemme og køre bil.");
+}
